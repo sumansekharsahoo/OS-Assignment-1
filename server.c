bc084c94f5d4522220ac147f7725e4f302d41d41
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2023-09-29 01:16:10 +0530

    added msgsnd to server.c

--- server.c
+++ server.c
@@ -21,34 +21,84 @@ int main()
 {
     int msgqid;
     key_t key;
-    if ((key = ftok("client.c", 85)) == -1)
+    int len;
+    pid_t cid;
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1)
     {
-        perror("ftok returned -1");
+        perror("Pipe creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((key = ftok("server.c", 85)) == -1)
+    {
+        perror("ftok returned -1\n");
         exit(1);
     }
-
     if ((msgqid = msgget(key, PERMS | IPC_CREAT)) == -1)
     {
-        PERROR("Failed to connect to Message queue");
+        perror("Failed to assign Message queue identifier\n");
         exit(1);
     }
 
+    printf("Server is waiting for the Client\n");
     while (true)
     {
-        msgbuf buf;
-        if (msgrcv(msgqid, &buf, sizeof(buf), 0, 0) == -1)
+        if (ppid != getppid())
         {
-            perror("Couldn't receive message from msgque");
-            exit(1);
+            exit(0);
         }
-        char menu = buf.mtext[0];
-        switch (menu)
+
+        if ((cid = fork()) == -1)
         {
-        case '1':
-            printf("Server pinged!");
-            break;
-        case '2':
-            // start here
+            perror("FORK");
+        }
+
+        if (cid == 0)
+        {
+            msgbuf buf;
+            if (msgrcv(msgqid, &buf, sizeof(buf), 0, 0) == -1)
+            {
+                perror("Couldn't receive message from msgque\n");
+                exit(1);
+            }
+            len = strlen(buf.mtext);
+            char choice = buf.mtext[0];
+            char filename[50];
+            switch (choice)
+            {
+            case '1':
+                buf.mtext = "Hello";
+                if (msgsnd(msgqid, &buf, sizeof(buf), 0) == -1)
+                {
+                    perror("Failed to send message");
+                    exit(1);
+                }
+                break;
+            case '2':
+                strncpy(filename, buf.mtext + 1, len - 1);
+                filename[len - 1] = '\0';
+
+                close(pipe_fd[0]);
+                dup2(pipe_fd[1], 1);
+                dup2(pipe_fd[1], 2);
+                close(pipe_fd[1]);
+
+                // exec
+                break;
+            }
+        case '3':
+            strncpy(filename, buf.mtext + 1, len - 1);
+            filename[len - 1] = '\0';
+
+            close(pipe_fd[0]);
+            dup2(pipe_fd[1], 1);
+            dup2(pipe_fd[1], 2);
+            close(pipe_fd[1]);
+
+            // exec
             break;
         }
     }
